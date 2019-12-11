e56c1b2e045f71511316671a233f04e6e1e2d0c3
jjkiii <jjk322@lehigh.edu>
2019-12-11 09:55:43 -0500

    added comments

--- GrainBoundApp.py
+++ GrainBoundApp.py
@@ -1,8 +1,9 @@
-#To run the Kivy application: 
+#To run the Kivy application: simply double click on the python script. easy.
 
 import kivy
 kivy.require('1.11.1') #current kivy version
 
+# These are the imported modules for kivy
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
@@ -10,7 +11,7 @@ from kivy.uix.button import Button
 from functools import partial
 from kivy.uix.textinput import TextInput
 
-
+# This is the beginning of our application's code
 class GBApp(App):
     def build(self):
         return Button(text="Welcome to GrainBound!", background_color=(176,224,230))
