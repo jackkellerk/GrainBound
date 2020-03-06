d3bde6531a6e2a33e5b581b613b181df42b477f7
jjkiii <jjk322@lehigh.edu>
2020-03-06 04:45:47 -0500

    added functionality to image altering

--- GrainBoundApp.py
+++ GrainBoundApp.py
@@ -4,6 +4,11 @@
 import kivy
 kivy.require('1.10.1') #current kivy version
 
+# For Image contrast, brightness, gamma changing
+import cv2
+import numpy as np
+import os
+
 from kivy.app import App
 
 from kivy.uix.label import Label
@@ -26,9 +31,6 @@ import DM3lib as dm3
 
 dm3f = dm3.DM3("example.dm3")
 print dm3f.info
-
-#TODO fix the graphics because I ruined it :(
-#https://kivy.org/doc/stable/api-kivy.uix.slider.html
 
 Builder.load_string("""
 <rootwi>:
@@ -57,6 +59,14 @@ Builder.load_string("""
             width: 225
             pos: 215, root.height-230
 
+        Image:
+            size_hint_y: None
+            source:"./material.jpg"
+            id: material
+            height: 600
+            width: 600
+            pos: 600, root.height-800
+
         Button:
             text: "Tool: Imaging"
             pos: root.width-160, root.height-85
@@ -76,18 +86,22 @@ Builder.load_string("""
             min: 0
             max: 100
             step: 1
-            value: 50.0
+            value: 0.0
             width: 200
             orientation: 'horizontal'
             pos: 100, root.height-700
+            on_value: root.brightnessUpdate()
 
         TextInput:
             text: str(brightnessSlider.value)
+            id: brightnessTextInput
             width: 50
+            multiline: False
             height: 30
             pos: 310, root.height-667.5
             background_color: (0.73,0.96,1.0,1) if self.focus else (0.93,0.93,0.93,1)
             background_normal: ''
+            on_text_validate: root.brightnessUpdate()
 
         Label:
             id: gammaLabel
@@ -100,18 +114,22 @@ Builder.load_string("""
             min: 0
             max: 100
             step: 1
-            value: 50.0
+            value: 0.0
             width: 200
             orientation: 'horizontal'
             pos: 100, root.height-600
+            on_value: root.gammaUpdate()
 
         TextInput:
             text: str(gammaSlider.value)
             width: 50
             height: 30
+            id: gammaTextInput
+            multiline: False
             pos: 310, root.height-567.5
             background_color: (0.73,0.96,1.0,1) if self.focus else (0.93,0.93,0.93,1)
             background_normal: ''
+            on_text_validate: root.gammaUpdate()
 
         Label:
             id: contrastLabel
@@ -124,18 +142,22 @@ Builder.load_string("""
             min: 0
             max: 100
             step: 1
-            value: 50.0
+            value: 0.0
             width: 200
             orientation: 'horizontal'
             pos: 100, root.height-800
+            on_value: root.contrastUpdate()
 
         TextInput:
             text: str(contrastSlider.value)
             width: 50
             height: 30
+            id: contrastTextInput
+            multiline: False
             pos: 310, root.height-767.5
             background_color: (0.73,0.96,1.0,1) if self.focus else (0.93,0.93,0.93,1)
             background_normal: ''
+            on_text_validate: root.contrastUpdate()
 
         Button:
             text: "Import Data"
@@ -152,14 +174,6 @@ Builder.load_string("""
             background_color: (0.96, 0.55, 0.66, 1.0)
             background_normal: ''
             pos: 100, root.height-885
-
-        Button:
-            text: "Run!"
-            width: 150
-            height: 75
-            background_color: (0.96, 0.55, 0.66, 1.0)
-            background_normal: ''
-            pos: root.width-160, 10
 
 """)
 
@@ -180,22 +194,77 @@ class CustomLayout(GridLayout):
 
 
 class rootwi(GridLayout):
-    pass
+
+    def adjust_gamma(image, gamma=1.0):
+
+        invGamma = 1.0 / gamma
+        table = np.array([((i / 255.0) ** invGamma) * 255
+            for i in np.arange(0, 256)]).astype("uint8")
+
+        return cv2.LUT(image, table)
+
+    def contrastUpdate(self):
+        try:
+            # Make sure the input is a float
+            self.ids.contrastSlider.value = float(self.ids.contrastTextInput.text)
+
+            # Edit the image
+            img = cv2.imread("material.jpg")
+            alpha = (2.0 * (self.ids.contrastSlider.value / 100)) + 1.0 # Contrast control (1.0-3.0)
+            adjusted = cv2.convertScaleAbs(img, alpha=alpha, beta=self.ids.brightnessSlider.value)
+            cv2.imwrite("./out.png", adjusted)
+
+            # Reassign the source of the image
+            self.ids.material.source = "./out.png"
+            self.ids.material.reload()
+        except:
+            # If input is not a float, reset text to original value
+            self.ids.contrastTextInput.text = str(self.ids.contrastSlider.value)
+
+    def brightnessUpdate(self):
+        try:
+            # Make sure the input is a float
+            self.ids.brightnessSlider.value = float(self.ids.brightnessTextInput.text)
+
+            # Edit the image
+            img = cv2.imread("material.jpg")
+            beta = self.ids.brightnessSlider.value # Contrast control (1.0-3.0)
+            adjusted = cv2.convertScaleAbs(img, alpha=((2.0 * (self.ids.contrastSlider.value / 100)) + 1.0), beta=beta)
+            cv2.imwrite("./out.png", adjusted)
+
+            # Reassign the source of the image
+            self.ids.material.source = "./out.png"
+            self.ids.material.reload()
+        except:
+            # If input is not a float, reset text to original value
+            self.ids.brightnessTextInput.text = str(self.ids.brightnessSlider.value)
+
+    def gammaUpdate(self):
+        try:
+            # Make sure the input is a float
+            self.ids.gammaSlider.value = float(self.ids.gammaTextInput.text) 
+
+            # Edit the image
+            img = cv2.imread("material.jpg")
+            gamma = 0.2 #self.ids.gammaSlider.value / 100 # Contrast control (1.0-3.0)
+            adjusted = adjust_gamma(img, gamma=gamma)
+            cv2.imwrite("./out.png", adjusted)
+
+            # Reassign the source of the image
+            self.ids.material.source = "./out.png"
+            self.ids.material.reload()
+        except:
+            # If input is not a float, reset text to original value
+            self.ids.gammaTextInput.text = str(self.ids.gammaSlider.value)
 
 class MyApp(App):
     def build(self):
+        # Eventually make a method here to delete the temp directory
         return rootwi()
-
 
 class GBApp(App):
     def build(self):
         return rootwi()
-
-    #def build(self):   
-        #return Image(source='/Users/nkt/Desktop/GrainBound/GrainBound_Logo.png')
-        #Label(text="GrainBound", font_size=50)
-        #return Button(text="Welcome to GrainBound!", font_size=50, background_color=(175,238,238))
-
 
 if __name__ == "__main__":
     GBApp().run()
